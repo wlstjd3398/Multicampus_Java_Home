bb0629774b9ea21083da2e99115d0f077bfde8b8
wlstjd3398 <wlstjd3398@naver.com>
2023-05-24 00:29:03 +0900

    1일차

---
+++ myJava/src/chap01/Hello.java
@@ -0,0 +1,13 @@
+package chap01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World~!");
+		System.out.println("¾È³ç ¼¼»ó¾Æ~!");
+		
+	}
+
+}
