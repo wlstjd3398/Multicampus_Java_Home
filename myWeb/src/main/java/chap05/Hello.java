2928fc8fe087aead753211caaa8bebf35c122e21
wlstjd3398 <wlstjd3398@naver.com>
2023-08-30 17:54:25 +0900

    jsp시작

---
+++ myWeb/src/main/java/chap05/Hello.java
@@ -0,0 +1,46 @@
+package chap05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet(description = "My First Servlet", urlPatterns = { "/chap05/hello" })
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.append("<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><title>Hello</title></head>"
+				+ "<body><h2>Hello, World ... !</h2><hr />").append("<h4>현재 날짜와 시간: " + java.time.LocalDateTime.now() + "</h4></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
